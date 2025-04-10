8edfeb430399dbf448a42c25f306f19a8e33a4b2
enessmr <enessmr22@gmail.com>
2025-04-10 20:51:56 +0300

    add conversions
    end-4 merge :3

--- .config/ags/modules/sideleft/toolbox.js
+++ .config/ags/modules/sideleft/toolbox.js
@@ -3,6 +3,7 @@ const { Box, Label, Scrollable } = Widget;
 import QuickScripts from './tools/quickscripts.js';
 import ColorPicker from './tools/colorpicker.js';
 import Configure from './../sideleft/tools/configure.js';
+import Conversions from './tools/conversions.js';
 export default Scrollable({
     hscroll: "never",
     vscroll: "automatic",
@@ -12,6 +13,7 @@ export default Scrollable({
         children: [
             Configure(),
             ColorPicker(),
+            Conversions(),
             QuickScripts(),
         ]
     })
